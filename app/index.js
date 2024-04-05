fa20d825f478350ea57cf0641a70155713586f1c
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2024-04-05 13:52:55 +0200

    Added clock + data transfer toggle

--- app/index.js
+++ app/index.js
@@ -1,7 +1,10 @@
 import * as document from "document";
 import * as messaging from "messaging";
 import { HeartRateSensor } from "heart-rate";
+import { clock } from "clock";
 import { me as appbit } from "appbit";
+
+let sendingData = false;
 
 if (!appbit.permissions.granted("access_heart_rate")) {
     console.log("We're not allowed to read a users' heart rate!");
@@ -15,6 +18,7 @@ messaging.peerSocket.addEventListener("error", (err) => {
     console.error(`Connection error: ${err.code} - ${err.message}`);
 })
 
+// Heart Rate
 function sendHeartRateData(heartRate) {
     const data = {
         heartRate: heartRate
@@ -30,9 +34,39 @@ if (HeartRateSensor) {
     const hrm = new HeartRateSensor({ frequency: 1});
     hrm.addEventListener("reading", () => {
         element.text = hrm.heartRate;
-        sendHeartRateData(hrm.heartRate);
+        if (sendingData) { sendHeartRateData(hrm.heartRate); };
     });
     hrm.start();
 } else {
     console.log("This device does not have a HeartRateSeonsor!");
 }
+
+// Clock
+clock.granularity = "seconds";
+
+const clockElement = document.getElementById("clock");
+
+clock.addEventListener("tick", (evt) => {
+    const today = evt.date;
+    const hours = today.getHours();
+    const mins = today.getMinutes();
+    const secs = today.getSeconds();
+    clockElement.text = `${hours}:${mins}:${secs}`;
+});
+
+// Toggle data transfer
+const buttonElement = document.getElementById("dataToggle");
+const backgroundElement = document.getElementById("background");
+const statusElement = document.getElementById("status");
+
+buttonElement.addEventListener("click", (evt) => {
+    sendingData = !sendingData;
+
+    if (sendingData) {
+        backgroundElement.style.fill = "green";
+        statusElement.text = "Transmitting data"
+    } else {
+        backgroundElement.style.fill = "red";
+        statusElement.text = "Tab to send data"
+    }
+})
