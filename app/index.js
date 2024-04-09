e849e8119063bfed848918514d74b5d11594321f
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2024-04-09 10:54:31 +0200

    Send dateTime to companion app

--- app/index.js
+++ app/index.js
@@ -18,11 +18,60 @@ messaging.peerSocket.addEventListener("error", (err) => {
     console.error(`Connection error: ${err.code} - ${err.message}`);
 })
 
+function formatTime(date, clock = false) {
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
+    let hours = date.getHours();
+    let mins = date.getMinutes();
+    let secs = date.getSeconds();
+
+    if (month < 10) {
+        month = `0${month}`;
+    }
+
+    if (day < 10) {
+        day = `0${day}`;
+    }
+
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+
+    if (mins < 10) {
+        mins = `0${mins}`;
+    }
+
+    if (secs < 10) {
+        secs = `0${secs}`;
+    }
+
+    if (clock) {
+        return `${hours}:${mins}:${secs}`;
+    }
+
+    return `${year}-${month}-${day} ${hours}:${mins}:${secs}`;
+}
+
+// Clock
+clock.granularity = "seconds";
+
+const clockElement = document.getElementById("clock");
+let today;
+
+clock.addEventListener("tick", (evt) => {
+    today = evt.date;
+    clockElement.text = formatTime(today, true);
+});
+
+
 // Heart Rate
 function sendHeartRateData(heartRate) {
     const data = {
+        dateTime: formatTime(today),
         heartRate: heartRate
     }
+    
     if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
         messaging.peerSocket.send(data);
     }
@@ -32,6 +81,7 @@ const element = document.getElementById("heartRate");
 
 if (HeartRateSensor) {
     const hrm = new HeartRateSensor({ frequency: 1});
+    element.text = "--";
     hrm.addEventListener("reading", () => {
         element.text = hrm.heartRate;
         if (sendingData) { sendHeartRateData(hrm.heartRate); };
@@ -40,19 +90,6 @@ if (HeartRateSensor) {
 } else {
     console.log("This device does not have a HeartRateSeonsor!");
 }
-
-// Clock
-clock.granularity = "seconds";
-
-const clockElement = document.getElementById("clock");
-
-clock.addEventListener("tick", (evt) => {
-    const today = evt.date;
-    const hours = today.getHours();
-    const mins = today.getMinutes();
-    const secs = today.getSeconds();
-    clockElement.text = `${hours}:${mins}:${secs}`;
-});
 
 // Toggle data transfer
 const buttonElement = document.getElementById("dataToggle");
